501e9be4f2faa12d1ce001b5ea07bb0717f04925
EltonVa <163590724+EltonVa@users.noreply.github.com>
2024-03-15 15:07:01 -0500

    Add files via upload

---
+++ contact-form.php
@@ -0,0 +1,20 @@
+<?php
+ 
+$name=$_POST['name'];
+$visitor_email=$_POST['email'];
+$message=$_POST['message'];
+
+$email_form='firealarmsecurity@zedy3d2.com';
+
+$email_subject=`New form submission $name`;
+$email_body= "Uer Name: $name.\n".
+            "User Email: $visitor_email.\n".
+            "User message: $message.\n";
+
+$to ="firealarmscrt@gmail.com";
+$headers ="Form: $email_form\r\n";
+$headers ="Reply-To: $visitor_email\r\n";
+mail($to, $email_subject, $email_body, $headers);
+header("Location: contact us.html");
+
+?>
