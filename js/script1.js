46fdb40f2f6dde64ae97ef869568f805ad61a7ed
EltonVa <163590724+EltonVa@users.noreply.github.com>
2024-03-15 15:08:26 -0500

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,5 @@
+/* JavaScript Doc */
+const years = new Date().getFullYear()
+document.getElementById('year').innerHTML = `Secure Technology Connection ${years}<br>&#169All right reserve<br>` 
+//
+
